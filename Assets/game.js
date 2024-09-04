a035f4c93d0a33ef20663e3148450f231addc0d9
AUG__DOG <augustdagostino@icloud.com>
2024-09-04 09:20:21 -0500

    Create game.js
    Testing

---
+++ Assets/game.js
@@ -0,0 +1,14 @@
+function yuji(Health, Attack, Defense, Speed, cursedEnergy) {
+  this.Health = 200;
+  this.Attack = 340;
+  this.Defense = 15;
+  this.speed = 50;
+  this.cursedEnergy = 1350;
+}
+function panda(Health, Attack, Defense, Speed, cursedEnergy) {
+  this.Health = 350;
+  this.Attack = 45;
+  this.Defense = 40;
+  this.Speed = 35;
+  this.cursedEnergy = 800;
+}
